195683b9a2e3863d8888b8eb6d3120179a634e75
Mansi <19mansilaad@gmail.com>
2022-01-20 12:54:57 +0530

    Closures added

---
+++ Closures.js
@@ -0,0 +1,30 @@
+function x() {
+    for (var i = 1; i <= 5; i++) {
+      function close(i) {
+        setTimeout(() => {
+          console.log(i);
+        }, i * 1000);
+      }
+      close(i);
+    }
+  }
+  //x();
+  
+  function multiply(num1, num2) {
+    if (num2 !== undefined) {
+      return num1 * num2;
+    }
+    return function doMultiply(num2) {
+      return num1 * num2;
+    };
+  }
+  console.log('Multiplication is ', multiply(2, 5));
+  const double = multiply(10);
+  console.log(double);
+  console.log(double(3));
+  
+  function arrayFromValue(item) {
+    return;
+    [item];
+  }
+  console.log(arrayFromValue(10));
