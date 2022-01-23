c2ed96c0409abd0d313e9805234a40fe5ab92580
Mansi <19mansilaad@gmail.com>
2022-01-23 18:36:26 +0530

    Detect pair equal to desired sum added

--- TwoNumbersEqualToDesiredSum.js
+++ TwoNumbersEqualToDesiredSum.js
@@ -2,6 +2,8 @@ let input = [3, 2, 5, 9, 4, 8];
 let dSum = 11;
 let set = new Set();
 
+let arr1=[2,4,6,0];
+
 for (let item of input) {
   let num = dSum - item;
   if (set.has(num)) {
@@ -11,16 +13,17 @@ for (let item of input) {
   set.add(item);
 }
 
+// 2) Naive approach - time complexity - O(n^2)
 function detectPair(sum, array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if (i == j) continue;
-      else if (array[i] + array[j] === sum) return [array[i], array[j]];
+  for (let i = 0; i < array.length-1; i++) {
+    for (let j = i+1; j < array.length; j++) {
+       if (array[i] + array[j] === sum) 
+         return [array[i], array[j]];
     }
   }
-  return null;
+  return ('Pair not found');
 }
-console.log(detectPair(dSum, input)); //return would be [12,16]
+console.log(detectPair(dSum, arr1)); 
 
 let result;
 
@@ -29,3 +32,18 @@ for (let i = 0; i < input.length; i++)
     if (i !== k && input[i] + input[k] == dSum) result = [input[i], input[k]];
 
 console.log(result);
+
+function findPair(sum, arr1) {
+  let arr=arr1.sort((a,b)=> { return a-b;});
+  let low= 0;
+  let high= arr.length-1;
+
+  while(low < high){
+    if (arr[low]+ arr[ high]=== sum){
+      return [arr[low], arr[high]];
+    }
+    (arr[low]+ arr[high]< sum)? low++: high--;
+  }
+  return ('Pair not found');
+}
+console.log(findPair(dSum, input)); 
